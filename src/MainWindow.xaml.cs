3141b4e7432f20af5a60d26be3ba6728ea69b9a8
The Satan Records <97181589+TheSatanRecords@users.noreply.github.com>
2022-03-12 18:07:23 +0100

    Add files via upload

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Microsoft.Web.WebView2.Wpf;
+
+namespace The_Satan_Records_Web_Audio_Player
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            gr.Children.Add(web);
+            web.Source = new System.Uri("https://thesatanrecords.github.io/audio_player.html");
+        }
+        public WebView2 web = new WebView2();
+    }
+}
